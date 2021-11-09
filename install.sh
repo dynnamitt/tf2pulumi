19d2bb215003655a2c76dea9fe17d53e8b2a1e02
Kjetil Midtlie <dynnamitt@users.noreply.github.com>
2021-11-09 18:59:45 +0100

    install.sh can now get the types (aws)

--- install.sh
+++ install.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 DEST=${1:-~/.local/bin}
 ln -v -s $(find "$(cd ..; pwd)" -type f -name tf2pulumi2) $DEST
+
+echo "Provider TYPES/cache download :"
+
+echo " - AWS ..."
+curl https://raw.githubusercontent.com/pulumi/pulumi-aws/master/provider/cmd/pulumi-resource-aws/schema.json \
+| jq '.types|keys' > .tf2pulumi2.aws.json
